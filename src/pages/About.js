16ca7aeddfa73e51f74ecde6a881f117725303c8
Johnny <jaani.matharu@gmail.com>
2022-03-07 16:48:57 -0500

    Font changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -66,17 +66,17 @@ export default function AboutMe() {
         <section>
       <MDBContainer id="size">
           <div className="backround">
-          <h3 className="headingBack" style={{ fontSize: '24px', fontFamily: 'Cambria', fontWeight: 'bold', color: 'white' }}>2021 projects</h3>
+          <h3 className="headingBack" style={{ fontSize: '24px', fontFamily: 'Tahoma', fontWeight: 'bold', color: 'white' }}>2021 projects</h3>
      
 <div id= "button">
   {/* add this to increase the icon size, style={{ fontSize: '24px' }} */}
-    <div id="height1"><h3 style={{fontWeight: "bold", fontSize: 24, fontFamily: 'Cambria', color: '00003a' }}>myBid</h3>
+    <div id="height1"><h3 style={{fontWeight: "bold", fontSize: 24, fontFamily: 'Tahoma', color: '00003a' }}>myBid</h3>
     <h6><a href='https://github.com/PrimalOrB/myBid'><i class="fab fa-github" style={{fontSize: '21px'}}></i></a>
     </h6><a href='https://mybid-live.herokuapp.com/'><img id ="pic1" src={pic1}></img></a></div>
-  <div id="height2"><h3 style={{fontWeight: "bold", fontSize: 24, fontFamily: 'Cambria', color: '00003a' }}>Tech Blog</h3>
+  <div id="height2"><h3 style={{fontWeight: "bold", fontSize: 24, fontFamily: 'Tahoma', color: '00003a' }}>Tech Blog</h3>
   <h6><a href='https://github.com/JohnyMatharu/Tech-Blog'><i class="fab fa-github" style={{ fontSize: '21px' }}></i></a>
   </h6><a href='https://stormy-castle-86583.herokuapp.com/'><img id ="pic2" src={pic2}></img></a></div>
-  <div id="height3"><h3 style={{fontWeight: "bold", fontSize: 24, fontFamily: 'Cambria', color: '00003a' }}>Maunny</h3>
+  <div id="height3"><h3 style={{fontWeight: "bold", fontSize: 24, fontFamily: 'Tahoma', color: '00003a' }}>Maunny</h3>
   <h6><a href='https://github.com/JohnyMatharu/Maunny/tree/main'><i class="fab fa-github" style={{ fontSize: '21px' }}></i></a>
   </h6><a href='https://johnymatharu.github.io/Maunny/'><img id ="pic3" src={pic3}></img></a></div>
   
